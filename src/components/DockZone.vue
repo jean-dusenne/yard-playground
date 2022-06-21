d42d168b9431ce5670b57a27ba312554def1d8bf
Jean DUSENNE <jean@dusenne.com>
2022-06-21 20:52:01 +0200

    feat: modifiy ui dockzone

--- src/components/DockZone.vue
+++ src/components/DockZone.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="dock-zone" :style="layoutZone">
-    <div v-for="slot in slots" :key="slot.id">{{ slot.id }}</div>
+    {{ props.dockName }}
+    <div class="container">
+      <div v-for="(slot, index) in slots" :key="slot.id" :class="{ occupied: isPrime(index + 1 + start) }">
+        <VTooltip>
+          <a>{{ slot.id }}</a>
+
+          <template #popper>
+            slot name: {{ slot.name }} <br />
+            status: {{ isPrime(index + 1) ? 'occupied' : 'free' }}
+          </template>
+        </VTooltip>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,14 +20,23 @@
 import { DockZoneModel } from '../models/DockZoneModel';
 import { computed } from 'vue';
 
-const props = defineProps<{ layout: DockZoneModel; counter: number; start: number }>();
+const props = defineProps<{ layout: DockZoneModel; counter: number; start: number; dockName: string }>();
 
 const layoutZone = computed(() => ({ ...props.layout }));
+
+const isPrime = (n: number): boolean => {
+  let start = 2;
+  const limit = Math.sqrt(n);
+  while (start <= limit) {
+    if (n % start++ < 1) return false;
+  }
+  return n > 1;
+};
 
 const slots = computed(() => {
   const list: any[] = [];
   for (let i = 1; i <= props.counter; i++) {
-    list.push({ id: props.start + i, name: `slot-start-${i}` });
+    list.push({ id: props.start + i, name: `slot-${props.start + i}` });
   }
   return list;
 });
@@ -23,24 +44,29 @@ const slots = computed(() => {
 
 <style scoped lang="scss">
 .dock-zone {
-  display: grid;
+  & > .container {
+    display: grid;
 
-  grid-template-columns: repeat(auto-fit, minmax(20px, 1fr));
-  grid-gap: 5px;
+    grid-template-columns: repeat(auto-fit, minmax(20px, 1fr));
+    grid-gap: 5px;
 
-  div {
-    cursor: pointer;
-    border: 1px solid red;
-    background: #fff;
-    white-space: nowrap;
-    height: v-bind('layoutZone.height');
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    &:hover {
-      background: #000;
-      color: #fff;
+    & > div {
+      cursor: pointer;
+      border: 1px solid red;
+      background: #fff;
+      white-space: nowrap;
+      height: v-bind('layoutZone.height');
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      &:hover {
+        background: #d1d1d1;
+        color: #fff;
+      }
+      &.occupied {
+        background: orange;
+      }
     }
   }
 }
--- src/components/YardPlayground.vue
+++ src/components/YardPlayground.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <DockZone :layout="dropZoneLayout" :counter="50" :start="0" />
-    <DockZone :layout="dropZoneLayout2" :counter="30" :start="50" />
-    <DockZone :layout="dropZoneLayout3" :counter="30" :start="80" />
-    <DockZone :layout="dropZoneLayout4" :counter="20" :start="110" />
+    <DockZone :layout="dropZoneLayout" :counter="50" :start="0" dock-name="Dock 001" />
+    <DockZone :layout="dropZoneLayout2" :counter="30" :start="50" dock-name="Dock 002" />
+    <DockZone :layout="dropZoneLayout3" :counter="32" :start="80" dock-name="Dock 003" />
+    <DockZone :layout="dropZoneLayout4" :counter="20" :start="112" dock-name="Parking" />
   </div>
 </template>
 
@@ -14,8 +14,8 @@ import { DockZoneModel } from '../models/DockZoneModel';
 
 const dropZoneLayout = ref<DockZoneModel>({ height: '6rem', left: '5%', top: '5%', width: '94%' });
 const dropZoneLayout2 = ref<DockZoneModel>({ height: '4rem', left: '30%', top: '30%', width: '69%' });
-const dropZoneLayout3 = ref<DockZoneModel>({ height: '4rem', left: '30%', top: '40%', width: '69%' });
-const dropZoneLayout4 = ref<DockZoneModel>({ height: '4.4rem', left: '5%', top: '20%', width: '10%' });
+const dropZoneLayout3 = ref<DockZoneModel>({ height: '4rem', left: '28%', top: '40%', width: '71%' });
+const dropZoneLayout4 = ref<DockZoneModel>({ height: '5rem', left: '5%', top: '20%', width: '10%' });
 </script>
 
 <style scoped>
