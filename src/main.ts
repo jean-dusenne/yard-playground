5362d889b9d5edf7323ca483877ee2a78550717d
Jean Dusenne <dev@dusenne.com>
2022-06-30 18:52:38 +0200

    feat: add hotjar

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,13 @@ import App from './App.vue';
 import FloatingVue from 'floating-vue';
 import 'sanitize.css';
 import 'floating-vue/dist/style.css';
+import VueHotjar from 'vue-hotjar-next';
 
-createApp(App).use(FloatingVue).mount('#app');
+createApp(App)
+  .use(VueHotjar, {
+    id: 3046255,
+    isProduction: true,
+    snippetVersion: 6
+  })
+  .use(FloatingVue)
+  .mount('#app');
