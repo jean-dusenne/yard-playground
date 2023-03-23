f93e55546413fc0eb699347faed5aaa1024233dd
Jean Dusenne <jean@dusenne.com>
2023-03-23 23:17:48 +0100

    fix: update start dock zone

--- src/components/YardPlayground.vue
+++ src/components/YardPlayground.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="yard">
     <DockZone :layout="dropZoneLayout" slot-height="6rem" :counter="38" :start="1" dock-name="Area 001" />
-    <DockZone :layout="dropZoneLayout2" slot-height="6rem" :counter="25" :start="37" dock-name="Area 002" />
-    <DockZone :layout="dropZoneLayout3" slot-height="6rem" :counter="28" :start="62" dock-name="Area 003" />
-    <DockZone :layout="dropZoneLayout4" slot-height="6rem" :counter="28" :start="90" dock-name="Parking" />
+    <DockZone :layout="dropZoneLayout2" slot-height="6rem" :counter="25" :start="40" dock-name="Area 002" />
+    <DockZone :layout="dropZoneLayout3" slot-height="6rem" :counter="28" :start="66" dock-name="Area 003" />
+    <DockZone :layout="dropZoneLayout4" slot-height="6rem" :counter="28" :start="95" dock-name="Parking" />
     <DockZone :layout="dropZoneLayout5" slot-height="25px" :counter="15" :start="131" dock-name="Vertical area" :vertical="true" />
   </div>
 </template>
